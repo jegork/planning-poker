bbbe7ea4f6a9966545c357d0c01ac5ef20d9bf2a
Jegor Kitskerkin <jegor.kitskerkin@gmail.com>
2023-05-10 19:36:41 +0300

    Add login screen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,125 +1,57 @@
 import {
   Box,
+  Button,
   Center,
+  Divider,
   Flex,
-  Grid,
-  GridItem,
-  Stack,
-  Text,
+  Heading,
+  Input,
+  Stack, Text,
 } from '@chakra-ui/react';
-import PokerCard from '../components/PokerCard';
-import PlayersList from '../components/PlayersList';
-import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
-import SelectablePokerCard, {
-  SelectablePokerCardProps,
-} from '../components/SelectablePokerCard';
 
-const FIBONACCI = '1, 2, 3, 5, 8, 13, 21, 34, 55, 89'
-  .split(', ')
-  .map((v) => parseInt(v));
-
-function getRandom(list: Array<any>) {
-  return list[Math.floor(Math.random() * list.length)];
-}
-
-interface Props {
-  playerVotes: Array<{ name: string; vote: number }>;
-  playersList: Array<string>;
-}
-
-export default function IndexPage({
-  playerVotes = [],
-  playersList = [],
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  const handleSelectCard: SelectablePokerCardProps['onClick'] = (v) => {};
-
+export default function IndexPage() {
   return (
-    <Box w="full" h="100vh" bg="gray.50" px={4} py={6}>
-      <Flex h="full" flexDir="column">
-        <Box flexGrow="1" w="full">
-          <Grid
-            h="full"
-            w="full"
-            gap="4"
-            gridTemplateColumns={'1fr 150px'}
-            gridTemplateRows={'200px 1fr'}
-            templateAreas={`
-            "table players"
-            "table stories"
-          `}
-          >
-            <GridItem area={'table'}>
-              <Stack
-                direction="column"
-                align="center"
-                justify="space-between"
-                h="full"
-              >
-                <Center flexGrow={1}>
-                  <Stack
-                    direction="row"
-                    spacing={8}
-                    bg="white"
-                    p={24}
-                    rounded="sm"
-                  >
-                    {playerVotes.map((v) => (
-                      <Box key={v.name}>
-                        <PokerCard value={v.vote} color={'green'} />
-                        <Text w="full" textAlign="center">
-                          {v.name}
-                        </Text>
-                      </Box>
-                    ))}
-                  </Stack>
-                </Center>
-                <Stack direction="row" spacing={6} w="full" justify="center">
-                  {FIBONACCI.map((v) => (
-                    <SelectablePokerCard
-                      key={v}
-                      value={v}
-                      color="red"
-                      onClick={handleSelectCard}
-                    />
-                  ))}
-                </Stack>
-              </Stack>
-            </GridItem>
+    <Box w="full" h="100vh" bgColor="#715DF2" bgImage="/background.svg" backgroundSize="cover">
+      <Center h="full" color="white">
+        <Stack dir="column" spacing={6}>
+          <Box>
+            <Heading size="md" textAlign="center">
+              Welcome to
+            </Heading>
+            <Heading size="lg" textAlign="center">
+              OpenPlanningPoker
+            </Heading>
+          </Box>
+          <Button size="lg" colorScheme="whiteAlpha" variant="solid">
+            Create a new game
+          </Button>
+          <Flex w="full" position="relative" py={2} align="center">
 
-            <GridItem area={'players'}>
-              <PlayersList players={playersList} />
-            </GridItem>
-
-            <GridItem area={'stories'}></GridItem>
-          </Grid>
-        </Box>
-      </Flex>
+            <Divider borderColor="white" borderWidth="1px"/>
+            <Text
+              px={2}
+              textTransform="uppercase"
+              fontSize="20px"
+              color="white"
+              fontWeight="semibold"
+            >
+              Or
+            </Text>
+            <Divider borderColor="white" borderWidth="1px" />
+          </Flex>
+          <Input
+            placeholder="Game code"
+            _placeholder={{ color: 'whiteAlpha.800' }}
+            size="lg"
+            fontWeight="bold"
+            textAlign="center"
+            _focusVisible={{}}
+          ></Input>
+          <Button size="lg" colorScheme="whiteAlpha" variant="solid">
+            Join an existing game
+          </Button>
+        </Stack>
+      </Center>
     </Box>
   );
 }
-
-export const getServerSideProps: GetServerSideProps<Props> = async (
-  context,
-) => {
-  const playersList = [
-    'Player 1',
-    'Player 2',
-    'Player 3',
-    'Player 4',
-    'Player 5',
-  ];
-
-  const playerVotes = playersList.map((v) => ({
-    name: v,
-    vote: getRandom(FIBONACCI),
-  }));
-
-  const props = {
-    playerVotes,
-    playersList,
-  };
-
-  return {
-    props: props,
-  };
-};
